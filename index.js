0de86e6f1d908bb2ea356b7499b7e5248c6d7dfe
Adrian <adrian.amoke@gmail.com>
2025-03-07 21:51:17 +0300

    edit

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(a, b) {
+    return a/b;
+}
+divide(2000, 100);
+
+const square = (x=2) => {
+    return x*x;
+}    
+square();
+
+const add = (a, b) => {
+    return a+b;
+}
+add(3, 4);
+
